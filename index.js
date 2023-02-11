d46c78236bdcae22ad78067bef4b5d219e8e76c1
MicWalczak <72381001+MicWalczak@users.noreply.github.com>
2023-02-11 20:58:18 +0100

    404

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.use(express.static("public"));
 app.get('/', (req,res)=>{
     res.sendFile(__dirname+"public/index.html")
 })
+app.get('*', function(req, res){
+  res.sendFile(__dirname+"/public/404.html")
+});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
