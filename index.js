0e875347575a2f935a33a07214ebd404fdee69cb
MicWalczak <72381001+MicWalczak@users.noreply.github.com>
2023-02-11 18:35:10 +0100

    first comit

---
+++ index.js
@@ -0,0 +1,24 @@
+const PORT = 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const e = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname+"public/index.html")
+})
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+  });
